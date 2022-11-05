c43f4d0ce700bb1ee52abd3b500fde89b4d7715c
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2022-11-05 22:27:07 +0530

    Folder structure improvements and updation of the icons

---
+++ app/src/main/java/com/amalwin/favdishapplication/views/fragments/FavoriteDishesFragment.kt
@@ -0,0 +1,24 @@
+package com.amalwin.favdishapplication.views.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FavoriteDishFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        TODO()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+    }
+}
