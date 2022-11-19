8ace3edb4dc0ee4a41046162ebe7beb135ab0320
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2022-11-19 23:38:38 +0530

    Pulling the random dish from remote server and inserting in to application

--- app/src/main/java/com/amalwin/favdishapplication/model/network/FavDishesAPI.kt
+++ app/src/main/java/com/amalwin/favdishapplication/model/network/FavDishesAPI.kt
@@ -2,8 +2,9 @@ package com.amalwin.favdishapplication.model.network
 
 import com.amalwin.favdishapplication.model.entities.RandomDish
 import io.reactivex.rxjava3.core.Single
-import retrofit.http.GET
-import retrofit.http.Query
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 interface FavDishesAPI {
     @GET("/recipes/random")
--- app/src/main/java/com/amalwin/favdishapplication/model/network/FavDishesAPIService.kt
+++ app/src/main/java/com/amalwin/favdishapplication/model/network/FavDishesAPIService.kt
@@ -2,14 +2,15 @@ package com.amalwin.favdishapplication.model.network
 
 import com.amalwin.favdishapplication.model.entities.RandomDish
 import io.reactivex.rxjava3.core.Single
-import retrofit.GsonConverterFactory
-import retrofit.Retrofit
-import retrofit.RxJavaCallAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class FavDishesAPIService {
     private val favDishAPI = Retrofit.Builder().baseUrl("https://api.spoonacular.com/")
         .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
         .build().create(FavDishesAPI::class.java)
 
     fun fetchRandomDishes(
